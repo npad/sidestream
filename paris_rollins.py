3847658163cdcd5c0a79312565aeceaee134e2c6
Josh Bailey <joshb@google.com>
2015-12-06 20:24:12 -0800

    * Add Web100 polling. * Correct generation of log filenames to include original local port and hostname. * Increase traceroute timeout from 20 to 30s.

--- paris_rollins.py
+++ paris_rollins.py
@@ -8,11 +8,21 @@
 # can't use them because the M-Lab platform doesn't have them. This should
 # be revisited if the M-Lab platform is upgraded.
 
-# TODO(joshb): this version just provides functions to run paris-traceroute,
-# and manage a cache of recent IP addresses. Next step is to poll Web100 agent.
+# Needs to be run as root, with the Web100 libraries installed, in the NPAD
+# slice.
+#
+#   export PYTHONPATH=/home/iupui_npad/build/lib/python2.6/site-packages ; \
+#   export LD_LIBRARY_PATH=/home/iupui_npad/build/lib ; \
+#   ./paris_rollins.py
+#
+# TODO(joshb): this is for experimental use only. The next step is to replace
+# the old wrapper with this one.
 
+from Web100 import *
 import os
 import multiprocessing
+import platform
+import re
 import subprocess
 import sys
 import time
@@ -25,21 +35,33 @@ TIMEOUT_BIN = '/usr/bin/timeout'
 WORKER_NICE = 19
 # paris-traceroute should take no longer than this to complete (timed out,
 # partial results will be discarded).
-WORKER_TIMEOUT = 20 
+WORKER_TIMEOUT = 30 
 # Maximum number of paris-traceoutes to run simultaneously (requests to run
 # more will be discarded).
 MAX_WORKERS = 10
+# Whether a TCP connection is closed.
+WEB100_STATE_CLOSED = 1
+# Whether a TCP connection was over IPv4
+WEB100_IPV4 = 1
+# Base source port to use when running traceroute
+PARIS_TRACEROUTE_SOURCE_PORT_BASE = 33457
+# Where to log traceroutes
+LOG_PATH = '/tmp'
+# Do not traceroute to an IP more than once in this many seconds
+IP_CACHE_TIME_SECONDS = 120
+
 
 
 def log_worker(message):
   print time.strftime('%Y%m%d %T %%s', time.gmtime(time.time())) % message
 
 
-def make_log_file_name(log_file_root, log_time,
+def make_log_file_name(log_file_root, log_time, mlab_hostname,
                        remote_ip, remote_port, local_ip, local_port):
-  log_time = time.strftime('%Y/%m/%d/%Y%m%dT%TZ', time.gmtime(log_time))
+  time_fmt = '/'.join(('%Y/%m/%d', mlab_hostname, '%Y%m%dT%TZ'))
+  log_time = time.strftime(time_fmt, time.gmtime(log_time))
   log_ip = '-'.join((remote_ip, str(remote_port), local_ip, str(local_port)))
-  log_file_relative = ''.join((log_time, log_ip, '.paris'))
+  log_file_relative = ''.join((log_time, '-', log_ip, '.paris'))
   log_file = os.path.join(log_file_root, log_file_relative)
   return log_file
 
@@ -47,7 +69,7 @@ def make_log_file_name(log_file_root, log_time,
 # Try to run paris-traceroute and log output to a file. We assume any
 # errors are transient (Eg, temporarily out of disk space), so do not
 # crash if the run fails.
-def run_worker(log_file_root, log_time,
+def run_worker(log_file_root, log_time, mlab_hostname, traceroute_port,
                remote_ip, remote_port, local_ip, local_port):
   os.nice(WORKER_NICE)
   command = (
@@ -57,14 +79,15 @@ def run_worker(log_file_root, log_time,
     '--algo=exhaustive',
     '-picmp',
     '-s',
-    str(local_port),
+    str(traceroute_port),
     '-d',
     str(remote_port),
     remote_ip)
   log_command = ' '.join(command)
   log_worker(log_command)
   log_file_name = make_log_file_name(
-    log_file_root, log_time, remote_ip, remote_port, local_ip, local_port)
+    log_file_root, log_time, mlab_hostname,
+    remote_ip, remote_port, local_ip, local_port)
   log_file_dir = os.path.dirname(log_file_name)
   if not os.path.exists(log_file_dir):
     try:
@@ -90,6 +113,7 @@ def run_worker(log_file_root, log_time,
     log_worker('could not run %s' % log_command)
     return False
   return True
+
 
 # Test if an IP address has been seen within the timeout period.
 class RecentIPAddressCache(object):
@@ -126,6 +150,7 @@ class RecentIPAddressCache(object):
     self.expire(now)
     return address in self.address_cache
 
+
 # Manage a pool of worker subprocessors to run traceoutes in.
 class ParisTraceroutePool(object):
 
@@ -134,24 +159,65 @@ class ParisTraceroutePool(object):
     self.log_file_root = log_file_root
     self.busy = []
 
-  def update_busy(self):
+  def busy_workers(self):
     self.busy = [result for result in self.busy if result.ready() == False]
+    return len(self.busy)
 
   # Return true if we have capacity to run more traceroutes.
   def free(self):
-    self.update_busy()
-    return len(self.busy) < MAX_WORKERS
+    return self.busy_workers() < MAX_WORKERS
 
   # Return true if no workers running.
   def idle(self):
-    self.update_busy()
-    return len(self.busy) == 0
+    return self.busy_workers() == 0 
 
   # Return true if we have spare capacity and we scheduled a traceroute.
-  def run_async(self, log_time, remote_ip, remote_port, local_ip, local_port):
+  def run_async(self, log_time, mlab_hostname, traceroute_port,
+                remote_ip, remote_port, local_ip, local_port):
     if self.free():
       self.busy.append(self.pool.apply_async(run_worker,
-        args=(self.log_file_root, log_time,
+        args=(self.log_file_root, log_time, mlab_hostname, traceroute_port,
               remote_ip, remote_port, local_ip, local_port)))
       return True
     return False
+
+
+def uncached_closed_connections(agent, recent_ip_cache):
+   closed_connections = [] 
+   for connection in agent.all_connections():
+     state = connection.read('State')
+     remote_ip = connection.read('RemAddress')
+     address_type = connection.read('LocalAddressType')
+
+     if (state == WEB100_STATE_CLOSED and
+         address_type == WEB100_IPV4 and
+         not recent_ip_cache.cached(remote_ip)):
+         recent_ip_cache.add(remote_ip)
+         log_time = time.time()
+         remote_port = connection.read('RemPort')
+         local_ip = connection.read('LocalAddress')
+         local_port = connection.read('LocalPort')
+         closed_connections.append((
+             log_time, remote_ip, remote_port, local_ip, local_port))
+   return closed_connections
+
+
+def get_mlab_hostname():
+   mlab_pattern = re.compile('^(mlab\d+\.[a-z]{3,3}\d+)')
+   mlab_hostname = mlab_pattern.match(platform.node()).group(1)
+   return mlab_hostname
+
+           
+if __name__ == '__main__':
+    mlab_hostname = get_mlab_hostname()
+    agent = Web100Agent()
+    recent_ip_cache = RecentIPAddressCache(IP_CACHE_TIME_SECONDS)
+    pool = ParisTraceroutePool(LOG_PATH)
+
+    while True:
+      connections = uncached_closed_connections(agent, recent_ip_cache)
+      for log_time, remote_ip, remote_port, local_ip, local_port in connections:
+          traceroute_port = PARIS_TRACEROUTE_SOURCE_PORT_BASE + pool.busy_workers()
+          pool.run_async(log_time, mlab_hostname, traceroute_port,
+                         remote_ip, remote_port, local_ip, traceroute_port)
+      time.sleep(5)
--- paris_rollins_test.py
+++ paris_rollins_test.py
@@ -25,15 +25,18 @@ class ParisRollinsTestCase(unittest.TestCase):
 
   def test_run(self):
     pool = paris_rollins.ParisTraceroutePool(self.tmpdir)
+    test_hostname = 'test.host'
     local_ip = '127.0.0.1'
+    local_port = 6666
     base_local_port = 33457
     remote_ip = self.TEST_DEST_IP
     remote_port = 9999
     # run full complement of workers, repeatedly.
     for log_time in range(5):
       workers = 0
-      while pool.run_async(log_time, remote_ip, remote_port,
-                           local_ip, base_local_port + workers):
+      traceroute_port = base_local_port + workers
+      while pool.run_async(log_time, test_hostname, traceroute_port,
+                           remote_ip, remote_port, local_ip, local_port):
         workers += 1
       # should be able to launch parallel traceroutes
       self.assertTrue(workers > 2)
@@ -42,19 +45,19 @@ class ParisRollinsTestCase(unittest.TestCase):
         time.sleep(1)
       # ensure all log files exist and have expected contents.
       for worker in range(workers):
-        local_port = base_local_port + worker
         expected_log = os.path.join(
           self.tmpdir,
           paris_rollins.make_log_file_name(self.tmpdir, log_time,
+                                           test_hostname,
                                            remote_ip, remote_port,
                                            local_ip, local_port))
         self.assertTrue(os.path.isfile(expected_log))
         self.assertTrue(os.path.getsize(expected_log) > 0)
         expected_log_header = re.compile(
           'traceroute \[\(%s:%u\) -> \(%s:%u\)\], protocol icmp, algo exhaustive' % (
-              '([\d\.]+)', local_port, remote_ip, remote_port))
+              '([\d\.]+)', traceroute_port, remote_ip, remote_port))
         log_contents = open(expected_log).read()
-        self.assertEquals(1, len(expected_log_header.match(log_contents).groups()))
+        self.assertTrue(expected_log_header.match(log_contents) is not None)
 
   def test_recentcache(self):
     ip = self.TEST_DEST_IP 
