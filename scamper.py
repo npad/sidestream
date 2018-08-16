a5abe4ea3373f5947dc08615d6d74881743c9b43
yachang <changya@gmail.com>
2018-08-16 12:00:04 -0400

    fix

--- scamper.py
+++ scamper.py
@@ -238,10 +238,11 @@ def parse_ss_line(line, connections):
     state, _, local_ip_port, remote_ip_port = fields
     if state[-1] == '0':
       state = state[:-1]
-  else if len(fields) == 5:
+  elif len(fields) == 5:
     state, _, _, local_ip_port, remote_ip_port = fields
   else:
     log_worker('bad line: %s' % line)
+    return
 
   local_ip_fields = local_ip_port.rsplit(':', 1)
   if len(local_ip_fields) != 2:
